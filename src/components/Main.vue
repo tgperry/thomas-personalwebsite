95246bdc7821ce29c7a7accc27a73ccb3c888b94
tgperry <thomasgperry1999@gmail.com>
2021-03-16 18:36:40 -0600

    First quarter 2021 adjustments

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="text-column">
         <h1 class="my-name">Thomas Perry</h1>
-        <p class="some-description">+1 425 753 1991 | thomasperry@mines.edu</p>
+        <p class="some-description">+1 425 753 1991 | thomasperry@alumni.mines.edu</p>
         <div class="links">
             <a class="github" target="_blank" href="https://github.com/tgperry">GitHub</a>
             <a class="itch" target="_blank" href="https://thomasgp.itch.io/">Indie Games</a>
@@ -9,29 +9,27 @@
         </div>
         
         <h1 class="about-title">About Me</h1>
-        <p class="about">I am currently completing my Senior year at Colorado School of Mines pursuing a BS in Computer Science with an emphasis in Data Science. Come January, I will begin a full time career as an Open Technology Solutions Consultant at Credera in Denver, Colorado. My experience in jobs, education, and general life has consistently involved creativity, motivation, and high spirits. In my free time I work on personal web and mobile app projects, game development, my culinary skills, and getting outdoors whenever I can. Thank you for visiting my website, I hope it gives you a little better of an idea about myself! If you ever wish to contact me, please do not hesitate from using any of the methods above, I would be happy to hear from you to chat about anything.</p>
+        <p class="about">I recently achieved my undergraduate degree in Computer Science with an emphasis in Data Science at the Colorado School of Mines and now hold a role as an Open Technology Solutions Consultant at Credera in Denver, Colorado. My experience in jobs, education, and general life has consistently involved creativity, motivation, and high spirits. In my free time I work on personal web and mobile app projects, game development, my culinary skills, and getting outdoors whenever I can. Thank you for visiting my website, I hope it gives you a little better of an idea about myself! If you ever wish to contact me, please do not hesitate from using any of the methods above, I would be happy to hear from you to chat about anything.</p>
         
         <h1 class="education-title">Education</h1>
         <p class="education">Education is at the core of what makes me, me. I have had the distinct privilege of receiving some of the best education possible, from elementary school to my undergrad, and have been supported by the teachers I have met from every level the entire way.</p>
         <h1 class="school-title">Colorado School of Mines - Golden, Colorado, US</h1>
-        <p class="graduating">B.S. Computer Science, Emphasis in Data Science<br>August 2017 - December 2020 | Cumulative GPA: 3.52</p>
+        <p class="graduating">B.S. Computer Science &amp; Data Science, Cum Laude<br>August 2017 - December 2020</p>
         <p class="classes">
-            Here is a list of Computer Science classes I am taking right now:
-            <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI423">Computer Simulations</a> (Python)
-            <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI470">Machine Learning</a> (Python)
-            <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI400">Programming Languages</a> (OCaml, JavaScript)
-            <br><br>
-            Here is a list of Computer Science classes I enjoyed in the past:
+            Here is a list of Computer Science classes I enjoyed while at the Colorado School of Mines:
             <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI370">Advanced Software Engineering</a> (Python)
             <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI406">Algorithms</a> (C++, Python)
             <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI341">Computer Organization</a> (C++, Python)
+            <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI423">Computer Simulations</a> (Python)
             <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI303">Data Science</a> (Python)
             <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI403">Databases</a> (SQL, PostgreSQL)
             <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI262">Data Structures</a> (C++)
             <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI477">Game Development</a> (GML, C#)
             <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=MATH332">Linear Algebra</a>
             <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI274">Linux OS</a> (Vim)
+            <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI470">Machine Learning</a> (Python)
             <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI261">Programming Concepts</a> (C++)
+            <br>-<a class="classes_link" target="_blank" href="https://catalog.mines.edu/search/?search=CSCI400">Programming Languages</a> (OCaml, JavaScript)
             <br><br>
             Here is a list of Computer Science classes I enjoyed while abroad in Oviedo, Spain: <br>
             (Links are to Mines equivalent courses)
@@ -50,7 +48,7 @@
         <p class="graduating">Department of Mathematics and Computer Science 
             <br>January 2019 - May 2019
             <br><br>
-            Aside from having an amazing time, gaining a deeper appreciation for the Spanish culture and language, traveling to places all over, and making lifelong friends, I had the incredible honor to collaborate with a group of extremely intelligent computer science students to develop a decentralized, person to person chat application using SOLID, a decentralized web technology under development by Sir Tim Berners-Lee. During that time, I even had the remarkable experience of communicating with Sir Berners-Lee over the SOLID chat room on Gitter. What an incredible experience to place the cherry on top of one of the best times of my life. I cannot wait to travel back to Asturias and see my friends there again.
+            Aside from having an amazing time, gaining a deeper appreciation for the Spanish culture and language, traveling to places all over, and making lifelong friends, I had the incredible honor to collaborate with a group of very intelligent computer science students to develop a decentralized, person to person chat application using SOLID, a web technology under development by Sir Tim Berners-Lee. During that time, I even had the remarkable experience of communicating with Sir Berners-Lee over the SOLID chat room on Gitter. What an incredible experience to place the cherry on top of one of the best times of my life. I cannot wait to travel back to Asturias and see my friends there again.
         </p>
 
         <h1 class="school-title">Woodinville High School - Woodinville, Washington, US</h1>
@@ -66,6 +64,13 @@
 
         <h1 class="experience-title">Experience</h1>
         <p class="experience">It has been an incredible privilege to work for the following companies and collaborate with teams that have helped me grow into the person and professional I am today. I owe it to these coworkers in the same way I owe it to my teachers and professors for giving me the mentorship and guidance to grow and get better at what I love to do.</p>
+        
+        <h1 class="work-title"><a class="work-title-link" target="_blank" href="https://www.credera.com/">Credera</a> - Denver, Colorado, US</h1>
+        <p class="job">Open Technology Solutions Consultant<br>January 2021 - Present</p>
+        <p class="description">
+            My work as a consultant with Credera allows me to continue doing what I love, which is the exploration and implementation of new and interesting technologies in the field of software. I am also incredibly excited to begin my career in Denver and look forward to exploring the city more in a safe and responsible manner amid the pandemic.
+        </p>
+
         <h1 class="work-title"><a class="work-title-link" target="_blank" href="https://agriculture.trimble.com/">Trimble, Inc.</a> - Westminster, Colorado, US (Remote)</h1>
         <p class="job">Software Engineering Intern (Agriculture Group, GNSS Group)<br>May 2020 - December 2020</p>
         <p class="description">
@@ -114,10 +119,8 @@
         <p class="design-description">This is me in pixel art, haha!</p>
 
         <h1 class="projects-title">What Am I Working On Right Now?</h1>
-        <p class="projects">Currently, I am finishing the last of my credits in order to complete my undergraduate degree, tutoring students in the evenings, working part time for Trimble, and working on some side projects
-            I am very excited about. Without giving away too much, my side projects include a website that serves video and audio content based on a user's preferences and a machine learning tool that transcribes lectures into
-            organized notes. Stay tuned for the results of these projects, as they're sure to be linked here. Oh, and of course I am preparing to take on a consulting role at Credera in downtown Denver as I near my graduation date. Wish me luck and know that I'm wishing
-            you the same in all of your endeavors! Here's a neat quote for your motivation:
+        <p class="projects">Currently, I am holding a role at Credera as a Software Consultant and working on some side projects. Without giving away too much, my side projects include a website that serves video and audio content based on a user's preferences and a machine learning tool that transcribes lectures into
+            organized notes. Stay tuned for the results of these projects, as they're sure to be linked here. Wish me luck and know that I'm wishing you the same in all of your endeavors! Here's a neat quote for your motivation:
         </p>
         <p class="quote">"If you view all of your problems as opportunities, eventually you'll run out of problems."</p>
         
@@ -129,7 +132,7 @@
             <thought-machine />
         </div>
 
-        <p class="copyright">Copyright &copy; 2020 Thomas Perry</p> 
+        <p class="copyright">Copyright &copy; 2021 Thomas Perry</p> 
     </div>
 </template>
 
